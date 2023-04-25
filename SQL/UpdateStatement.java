6130a95bf32aa2d3d48c78442c3b904cce158021
LyubomirD <111146307+LyubomirD@users.noreply.github.com>
2023-04-25 21:31:55 +0300

    Add files via upload

--- SQL/UpdateStatement.java
+++ SQL/UpdateStatement.java
@@ -5,26 +5,14 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class UpdateStatement {
+public class UpdateStatement extends StatementDuplicateCode{
     private final Scanner scanner;
     public UpdateStatement(Scanner scanner) {
         this.scanner = scanner;
     }
 
     public void getUpdate(Connection connection) throws SQLException {
-        setUpdate(connection);
-    }
-
-    private void setUpdate(Connection connection) throws SQLException {
-        Statement statement;
-        try {
-            statement = connection.createStatement();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        statement.executeUpdate(updateValues(scanner));
-        System.out.println("Table has been updated");
-        statement.close();
+        getStatementCommand(connection, updateValues(scanner), "Table has been updated");
     }
 
     private static String updateValues(Scanner scanner) {
@@ -34,7 +22,7 @@ public class UpdateStatement {
         String lastName = scanner.next();
         System.out.println("Select row that will receive the changes:");
         int id = scanner.nextInt();
-        return "UPDATE table2 SET first_name = '" + firstName +"', last_name = '" + lastName + "' WHERE id = " + id + ";";
+        return "UPDATE people SET first_name = '" + firstName +"', last_name = '" + lastName + "' WHERE id = " + id + ";";
     }
 
 }
